ca55b597b859be62391a3f708b4b9525c7cc0831
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-11-29 11:10:40 +0100

    add prettier

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,40 +1,21 @@
-import React from 'react';
-import { useEffect, useState } from "react"
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
   const [data, setData] = useState([])
-  console.log("data useState", data)
+  console.log('data useState', data)
   useEffect(() => {
-
-
-    fetch("http://localhost:8000/api-kasa/logements")
-      .then(res => res.json())
+    fetch('http://localhost:8000/api-kasa/logements')
+      .then((res) => res.json())
       .then((data) => {
         setData(data?.appartmentData)
-        console.log("data", data)
       })
-
   }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
