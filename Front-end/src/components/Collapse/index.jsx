357bec2e4f75424bb6edc557c7fe525ab3a833b4
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-12-02 17:42:03 +0100

    collapseComponenet: test arrow width cildren of panel

--- Front-end/src/components/Collapse/index.jsx
+++ Front-end/src/components/Collapse/index.jsx
@@ -10,38 +10,39 @@ import { StyledCollapseContainer, StyledPanel } from "./StyledCollapse"
 
 function CollapseAbout() {
     const [key, setActiveKey] = useState(0)
-    const [panel, setPanel] = useState(false)
+    const [openPanel, setPanel] = useState(false)
     console.log("key", key)
-    console.log("panel", panel)
+    console.log("panel", openPanel)
+    console.log(contentPanel, "content panel")
 
     return (
 
-        <StyledCollapseContainer accordion={false} value={panel}>
+        <StyledCollapseContainer accordion={false} >
 
             {
                 contentPanel.map(({ title, content }, index) =>
-                    <StyledPanel Key={`${key}`}
-                        value={panel} arrow={key === 0 ? "bottom-arrow" : "top-arrow"}
-                        collapsible={key === 0 && `disabled`}
-                        header={title} headerClass="my-header-class"
+                    <StyledPanel
+
+                        key={`${index}-${key}`}
+                        arrow={!openPanel ? "bottom-arrow" : "top-arrow"}
+                        header={title}
+                        headerClass="my-header-class"
                         className="title-collapse"
                         showArrow={true}
-                        onClick={() => setActiveKey(Math.random(Math.floor(key)) * 100)}
-                    >
+                        onChange={activeKey} onClick={() => setPanel(({ children }) => children && openPanel ? false : true)}>
                         {content}
                     </StyledPanel>
                 )
             }
-
-
-        </StyledCollapseContainer>
+        </StyledCollapseContainer >
     )
-    function openPanel(e) {
-
+    function activeKey(e) {
+        setActiveKey((Math.random(Math.floor(key)) * 100))
 
         console.log(" evenement on change")
 
 
     }
+
 }
 export default CollapseAbout
