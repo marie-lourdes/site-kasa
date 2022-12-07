e15e6ea7d5b2558f068d8b331d591fdc2ae2dda5
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-12-07 22:26:36 +0100

    page error: css font position

--- Front-end/src/bases/StyledLink.js
+++ Front-end/src/bases/StyledLink.js
@@ -1,13 +1,24 @@
 import { Link } from "react-router-dom"
 import styled from "styled-components"
 import colors from "../utils/colors"
+import devices_mediaQueries from "../utils/responsive-design/devices.js"
 
 const StyledLink = styled(Link)`
-color:${colors.primary};
-text-decoration: none;
-${({ $link404 }) => $link404 && `font-size: 18px`};
-&:hover {
-    text-decoration: underline;
-}
+    color:${colors.primary};
+    text-decoration: none;
+   
+    ${({ $link404 }) => $link404 && `
+    font-size:0.0625em;
+    position: relative;
+    bottom: 25px;
+    @media screen and ${devices_mediaQueries.mobile}{
+        bottom: 200px; 
+        font-size:0.146em;
+    
+
+    }`};
+    &:hover {
+        text-decoration: underline;
+    }
 `
 export default StyledLink
--- Front-end/src/pages/Page404/Page404.js
+++ Front-end/src/pages/Page404/Page404.js
@@ -1,24 +1,43 @@
 import styled from "styled-components"
 import colors from "../../utils/colors"
+import devices_mediaQueries from "../../utils/responsive-design/devices.js"
 
 const StyledPage404 = styled.main`
-font-size: 288px;
-color: ${colors.primary}.
-font-weight: 700;
-display: flex;
-flex-direction: column;
-justify-content: space-around;
-align-items: center;
-background: transparent;
-min-height: 100vh;
-& > div {
-    font-family: MontSerratBold;
-    margin-bottom: -12%;
-}
-span{
-    font-size: 36px;
-   
-}
+    font-size: 18em;
+    color: ${colors.primary}.
+    font-weight: 700;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    background: transparent;
+    min-height: 100vh;
+    position: relative;
+    @media screen and ${devices_mediaQueries.mobile} {
+        font-size: 6em;
+        top: 50px;
+    }
+    & > div {
+        font-family: MontSerratBold;
+        margin-bottom: -12%;
+        @media screen and ${devices_mediaQueries.mobile} {
+           
+        }
+    }
+    span {
+        font-size: 0.125em;
+        font-weight: bold;
+        position: relative;
+        bottom: 5px;
+
+        @media screen and ${devices_mediaQueries.mobile} {
+            font-size:0.1875em;
+            word-spacing: 0.23em;
+            text-align: center;
+            bottom: 130px;
+        }
+    
+    }
 `
 
 export default StyledPage404
