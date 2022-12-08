4c1acd416a7abe43e101e217619993c94c144081
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-12-08 14:14:45 +0100

    comments

--- Front-end/src/pages/Page404/index.jsx
+++ Front-end/src/pages/Page404/index.jsx
@@ -8,7 +8,7 @@ function Page404() {
 
         <StyledPage404>
             <div> 404</div>
-            <span className="text-oups"> Oups! La page que <span className="indent"></span> vous demandez n'existe pas</span>
+            <span className="text-oups"> Oups! La page que <span className="indent"></span> vous demandez n'existe pas.</span>
             <StyledLink to="/" $link404>Retourner sur la page d'accueil</StyledLink>
         </StyledPage404>
 
