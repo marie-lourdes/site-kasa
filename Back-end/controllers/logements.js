deb0ae4041e63fa0fbe099a1f9c94cd77577ff9b
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-12-10 22:33:01 +0100

    commit

--- Back-end/controllers/logements.js
+++ Back-end/controllers/logements.js
@@ -7,13 +7,12 @@ exports.getOneLocation = (req, res) => {
     try {
         const query = req.params.id
         const location = data.find(({ id }) => id === query)
-        console.log("oneAPPARTEMENT", location)
         res.status(200).json(location)
+        console.log("requête d'une location reussie")
 
     } catch (error) {
         res.status(404).json({ error: "page not found" })
-        console.log("requête appartement echouée")
-
+        console.log("requête d'une location echouée")
     }
 };
 // envoi de la reponse avec toutes les locations de l api
@@ -21,16 +20,12 @@ exports.getAllLocations = (req, res) => {
     try {
         const allLocations = data.map((element) => element)
         res.status(200).json({ allLocations })
-        console.log("requete des appartement reussie")
+        console.log("requete des locations reussie")
 
     } catch (error) {
         res.status(404).json({ error: "page not found" })
-        console.log("requête des appartement echouée")
+        console.log("requête des locations echouée")
     }
-
-
-
-
 };
 
 
