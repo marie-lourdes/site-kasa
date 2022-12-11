07670aadaf9944555ad1231846fe1fe1e9f429a1
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-12-11 22:52:02 +0100

    style

--- Front-end/src/layouts/Header/index.jsx
+++ Front-end/src/layouts/Header/index.jsx
@@ -6,27 +6,25 @@ import StyledHeader from './StyledHeader.js'
 import StyledLink from '../../bases/StyledLink'
 
 function Header() {
-  return (
-    <StyledHeader>
-      <div className="logo">
-        <Link to="/">
-          <img src={logo} alt="logo Kasa" />
-        </Link>
-      </div>
-      <nav>
-        <ul>
-          <li>
-            {' '}
-            <StyledLink to="/"> Accueil</StyledLink>
-          </li>
-          <li>
-            {' '}
-            <StyledLink to="/about"> A Propos</StyledLink>
-          </li>
-        </ul>
-      </nav>
-    </StyledHeader>
-  )
+    return (
+        <StyledHeader>
+            <div className="logo">
+                <Link to="/">
+                    <img src={logo} alt="logo Kasa" />
+                </Link>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <StyledLink to="/"> Accueil</StyledLink>
+                    </li>
+                    <li>
+                        <StyledLink to="/about"> A Propos</StyledLink>
+                    </li>
+                </ul>
+            </nav>
+        </StyledHeader>
+    )
 }
 
 export default Header
