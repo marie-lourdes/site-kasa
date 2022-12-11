0e70e2f8d0b542a84ec61549717c57ac620d339d
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-12-11 15:30:24 +0100

    merge back end validator data

---
+++ Back-end/model/logements.js
@@ -0,0 +1,40 @@
+// import du module  AJV node.js pour l implementation d une logique de validation des données  en json via les shema  et type en javascript
+const Ajv = require("ajv")
+
+const shema = {
+    type: "object",
+    properties: {
+        id: { type: "string" },
+        title: { type: "string" },
+        cover: { type: "string" },
+        pictures: {
+            type: "array",
+            items: { type: "string" },
+        },
+        description: { type: "string" },
+        host: {
+            type: "object",
+            properties: {
+                name: { type: "string" },
+                picture: { type: "string" },
+
+            }
+        },
+        rating: { type: "string" },
+        location: { type: "string" },
+        equipments: {
+            type: "array",
+            items: { type: "string" },
+        },
+        tags: {
+            type: "array",
+            items: { type: "string" },
+        },
+    },
+
+}
+
+//creation d 'une instance objet '
+const ajv = new Ajv();
+// objet sera compilé avec le structure de shema et ses validators
+module.exports = ajv.compile(shema)
