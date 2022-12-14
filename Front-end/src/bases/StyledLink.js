2d3c30d4cbbdc4611d7b38f8b865786c8be28760
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-12-14 14:00:47 +0100

    comments

--- Front-end/src/bases/StyledLink.js
+++ Front-end/src/bases/StyledLink.js
@@ -6,29 +6,16 @@ import devices_mediaQueries from '../utils/responsive-design/devices.js'
 const StyledLink = styled(Link)`
   color: ${colors.primary};
   text-decoration: none;
+  /* Lien maintenu active et sousligné*/
   ${({ $activeLinkHome, $locationHome }) => $activeLinkHome === "is-active" && $locationHome === "/" && `
   text-decoration: underline;  
   `
   };
   ${({ $activeLinkAbout, $location }) => $activeLinkAbout === "is-active" && $location === "/about" && `
-
   text-decoration: underline;  
-  `
+   `
   };
  
-
-@media screen and ${devices_mediaQueries.mobile} {
-    &: hover,&:active {
-    text - decoration: underline;
-  }
-}
-@media screen and ${devices_mediaQueries.tablet} {      
-      &: hover,&:active {
-    text - decoration: underline;
-  }
-}
-
-
   ${({ $link404 }) =>
     $link404 &&
     `
